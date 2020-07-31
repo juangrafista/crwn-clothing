31b236e373b558303c093f9928b6ffedb4f23718
juangrafista <juangrafista@gmail.com>
2020-07-31 15:44:51 -0400

    data normalization on shop data, modified collection-preview component

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,15 +1,5 @@
 import { createSelector } from 'reselect';
 
-import memoize from 'lodash.memoize';
-
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  women: 4,
-  men: 5,
-};
-
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
@@ -17,10 +7,13 @@ export const selectCollections = createSelector(
   (shop) => shop.collections
 );
 
-export const selectCollection = memoize((collectionUrlParam) =>
-  createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
-  )
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
 );
+
+export const selectCollection = (collectionUrlParam) =>
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
+  );
