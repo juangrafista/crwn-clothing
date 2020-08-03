165f5ce5f38e90d2cf33f6c79c51c5aedab7d266
juangrafista <juangrafista@gmail.com>
2020-08-03 16:17:02 -0400

    added styled components on cart-dropdown, collection-item and custom-button. Fixed scroll issue on safari for dropdown

---
+++ src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+import CustomButton from '../custom-button/custom-button.component';
+
+export const CartDropdownContainer = styled.div`
+  position: absolute;
+  width: 240px;
+  height: 340px;
+  min-height: 0px;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  border: 1px solid black;
+  background-color: white;
+  top: 90px;
+  right: 40px;
+  z-index: 5;
+`;
+
+export const CartDropdownButton = styled(CustomButton)`
+  margin-top: auto;
+`;
+
+export const EmptyMessageContainer = styled.span`
+  font-size: 18px;
+  margin: 50px auto;
+`;
+
+export const CartItemsContainer = styled.div`
+  height: 240px;
+  flex: 1;
+  overflow: scroll;
+  -webkit-overflow-scrolling: auto;
+`;
